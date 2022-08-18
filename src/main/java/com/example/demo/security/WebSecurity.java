a3b692df5825ce6051679a013a16465afd460233
David Marcos <david.13254@gmail.com>
2022-08-18 12:05:31 +0200

    security

--- src/main/java/com/example/demo/security/WebSecurity.java
+++ src/main/java/com/example/demo/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -11,7 +12,17 @@ public class WebSecurity {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+    	http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.OPTIONS, "/").authenticated()
+        .antMatchers(HttpMethod.GET,"/").authenticated()
+        .antMatchers(HttpMethod.POST,"/").authenticated()
+        .antMatchers(HttpMethod.PUT, "/").authenticated()
+        .antMatchers(HttpMethod.DELETE,"/**").hasRole("admin")
+        .anyRequest().authenticated()
+        .and().formLogin().permitAll()
+        .and().httpBasic(withDefaults());
         return http.build();
     }
 	
